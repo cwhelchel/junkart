764fd6c9df8406e85b72f649dadfcc5a4005218c
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-04-02 13:22:11 +0000

    added rails 3.0.4 files - try this with railsinstaller.org's rails stack

---
+++ branches/test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Junkart2::Application.config.secret_token = 'b9f2b2b6db2ce4dee67b19088a205e220558a9cd6689e345b015c9fc0538f848c610300769ae4b0585c435bacdf1e3f2517c63ae324c2f461c10a780b8e515d1'
