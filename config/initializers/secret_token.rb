4fd8abe1e1b9e7322d065e5596bf13ba110592f3
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-04-05 15:01:53 +0000

    let's redo all this crap... using rails installer w/ newer version of rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Junkart2::Application.config.secret_token = '837b2e2667f63ec9a9728524747291d95bf3805a926ea9c99be6d96a6bd03318eb1751dc718f9999730b559c31b622294ff1acaa517ea85ba5c643efdf850362'
