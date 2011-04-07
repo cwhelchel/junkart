80b62f53c7fc11f07d384542ad274d25bd8ab8e9
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-04-07 19:21:32 +0000

    added some seed test data. others: use "rake db:seed" to install this test data. !!!!!!! IT WILL DELETE EVERYTHING IN YOUR PRODUCTS TABLE IN YOUR DATABASE !!!!!!!!

--- branches/prod_scaffold/db/seeds.rb
+++ branches/prod_scaffold/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Product.delete_all
+
+Product.create(:name => 'Swirl Art #19',
+               :description => "Number 19 of the Swirl Art collection.",
+               :image => "swirlart19.jpg",
+               :price => 85.00
+               )
+
+
+Product.create(:name => 'Blue and Purple Flower (Large)',
+               :description => %{
+               A wonderful blue and purple flower. It's quite large.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+               veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+               velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+               cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+               est laborum.
+               },
+               :image => "lgblueandpurpleflower50.jpg",
+               :price => 85.00
+               )
