a200c319ecb07bfa1528a395b0f84f3a0b9174db
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-04-11 20:41:13 +0000

    modified the product model to link it to a category (via belongs_to) (also added code to get a category name TODO: move to better location; rails validator callback maybe) also added seed data for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 Product.delete_all
+Category.delete_all
 
 Product.create(:name => 'Swirl Art #19',
                :description => "Number 19 of the Swirl Art collection.",
@@ -29,3 +30,24 @@ Product.create(:name => 'Blue and Purple Flower (Large)',
                :image => "lgblueandpurpleflower50.jpg",
                :price => 85.00
                )
+
+Category.create(
+                :name => 'Junk Art',
+                :description => 'The default category all all of the art',
+                :image => 'rails.png'
+                )
+
+Category.create(
+                :name => 'Swirl Art',
+                :description => %{
+                A wonderful blue and purple flower. It's quite large.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                est laborum.
+                },
+                :image => 'rails.png'
+                )
