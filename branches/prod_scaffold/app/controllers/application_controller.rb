208171651dccddd9a2e1f106f1fea644f871c623
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-05-03 13:39:16 +0000

    added access restrictions: everything is restricted and the parts that end-users (i.e. store customers) will see have to be white listed with skip_before_filter

--- branches/prod_scaffold/app/controllers/application_controller.rb
+++ branches/prod_scaffold/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  # since everything is restricted, we have to whitelist stuff that doesn't
+  # need admin authorization
+  before_filter :authorize
+  
   protect_from_forgery
+  
+  protected
+    # The only users in the database are administrators (not end users) then
+    # if we find it in the db the that session belongs to an admin
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, :notice => "Please log in to access this area"
+      end
+    end
 end
