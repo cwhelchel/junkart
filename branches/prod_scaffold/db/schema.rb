020578e0a8649a599dabea8d42264b8c933751c8
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-05-04 15:17:38 +0000

    added quantity to line_items table

--- branches/prod_scaffold/db/schema.rb
+++ branches/prod_scaffold/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110503175922) do
+ActiveRecord::Schema.define(:version => 20110504124934) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20110503175922) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
