1424f07b702886e2298be9c981508ff91598f436
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-05-04 18:51:02 +0000

    use new render_notice helper render cart_summary partial in store_sort_actions (todo: rename this div)

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,9 @@
 module StoreHelper
+  def render_notice
+    if (flash[:notice])
+      return content_tag(:p, flash[:notice], :id => 'notice')
+    else
+      return content_tag(:p, '', :id => 'notice', :style => "display:none;")
+    end
+  end
 end
