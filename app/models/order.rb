cfe13216d20d3d2316ad76819d15956787e7b135
c.whelchel@gmail.com <c.whelchel@gmail.com@d1782662-f34a-16d6-0dcd-cb26d0eceb8d>
2011-05-04 20:18:37 +0000

    added the check-out "functionality" no credit-card processing or anything.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ActiveRecord::Base
+  validates_presence_of :name, :email, :address
+  has_many :line_items, :dependent => :destroy
+  
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |li|
+      li.cart_id = nil
+      line_items << li
+    end
+  end
 end
